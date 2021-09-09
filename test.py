2e97336c5e280df3ea8ff81cf63557da9d01ccb2
Fawzy20 <fawzy-elsam@outlook.fr>
2021-09-09 13:27:53 +0200

    j'ai contenu les donnée des email et username de l'url dans des variable et je les et afficher sur la console

--- test.py
+++ test.py
@@ -4,4 +4,8 @@ url = "https://random-data-api.com/api/internet_stuff/random_internet_stuff"
 
 reponse = requests.get(url)
 contenu = reponse.json()
-print(contenu.id)
+
+username = contenu['username']
+email = contenu['email']
+
+print("L'adresse email de l'utilisateur " + username + " est " + email)
