5784a9f60ea10a45ab75adb6c282398b02c42de8
Fawzy20 <fawzy-elsam@outlook.fr>
2021-09-10 21:27:27 +0200

    ajout de la function pour afficher l'os de l'utilisateur

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import requests
 
+
 url = "https://random-data-api.com/api/internet_stuff/random_internet_stuff"
 
 reponse = requests.get(url)
@@ -7,5 +8,31 @@ contenu = reponse.json()
 
 username = contenu['username']
 email = contenu['email']
+os = contenu['user_agent']
+windows = os.count('Windows')
+Linux = os.count('Linux')
+MacOS = os.count('Macintosh')
+Android = os.count('Android')
 
-print("L'adresse email de l'utilisateur " + username + " est " + email)
+
+if windows == 1 :
+
+    os_emojie = "Windows"
+
+elif Linux == 1 :
+    
+    os_emojie = "Linux"
+
+
+elif MacOS == 1 :
+
+    os_emojie = "MacOS"
+
+elif Android ==  1 :
+
+    os_emojie = "Android"
+
+else:
+    os_emojie = "Aucun OS reonnut"
+
+print("L'adresse email de l'utilisateur " +  username + " est " + email + " ." + " Iel utilise le système d'exploitation " + os_emojie + " . ")
