4db7ca704111f76ac5115c530b24b2cf39bcd2c1
Fawzy20 <fawzy-elsam@outlook.fr>
2021-09-11 11:09:32 +0200

    ajout condition erreur

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
 import requests
 
-
 url = "https://random-data-api.com/api/internet_stuff/random_internet_stuff"
 
 reponse = requests.get(url)
 contenu = reponse.json()
-
 username = contenu['username']
 email = contenu['email']
 os = contenu['user_agent']
@@ -14,25 +12,31 @@ Linux = os.count('Linux')
 MacOS = os.count('Macintosh')
 Android = os.count('Android')
 
+if(username != None or email != None):
 
-if windows == 1 :
+    if windows == 1:
 
-    os_emojie = "Windows"
+        os_emojie = "Windows"
 
-elif Linux == 1 :
-    
-    os_emojie = "Linux"
+    elif Linux == 1:
 
+        os_emojie = "Linux"
 
-elif MacOS == 1 :
+    elif MacOS == 1:
 
-    os_emojie = "MacOS"
+        os_emojie = "MacOS"
 
-elif Android ==  1 :
+    elif Android == 1:
 
-    os_emojie = "Android"
+        os_emojie = "Android"
+
+    else:
+        os_emojie = "Aucun OS reonnut"
 
 else:
-    os_emojie = "Aucun OS reonnut"
 
-print("L'adresse email de l'utilisateur " +  username + " est " + email + " ." + " Iel utilise le système d'exploitation " + os_emojie + " . ")
+    print("aucun user est reconnu")
+
+
+print("L'adresse email de l'utilisateur " + username + " est " + email +
+      " ." + " Iel utilise le système d'exploitation " + os_emojie + " . ")
